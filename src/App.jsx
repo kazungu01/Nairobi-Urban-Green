0a110507bd2503d09f625af7028489c3b2d8f5c0
kazungu01 <hassankazungu01@gmail.com>
2026-02-10 13:02:30 +0300

    updated HashRouter for easier easier nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,44 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; 
+import { Routes, Route } from "react-router-dom"
 
 // Components
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Carousel from "./Components/Carousel/Carousel"
 import Gallery from './Components/Gallery/Gallery'
-import KeyAchievements from './Components/KeyAchievements/KeyAchievements';
-import Team from './Components/Team/Team';
-import FAQ from './Components/FAQ/Faq';
-import Invitation from './Components/Invitation/Invitation';
-import Footer from './Components/Footer/Footer';
-import Blog from './Components/Blog/Blog';
-import BlogDetail from './Components/BlogDetail/BlogDetail';
-import ContactUs from './Components/ContactUs/ContactUs';
-import About from './Components/About/About';
-import Donate from './Components/Donate/Donate';
-import AdminLogin from './Components/Admin/AdminLogin';
-import AdminBlogs from './Components/Admin/AdminBlogs';
-
+import KeyAchievements from './Components/KeyAchievements/KeyAchievements'
+import Team from './Components/Team/Team'
+import FAQ from './Components/FAQ/Faq'
+import Invitation from './Components/Invitation/Invitation'
+import Footer from './Components/Footer/Footer'
+import Blog from './Components/Blog/Blog'
+import BlogDetail from './Components/BlogDetail/BlogDetail'
+import ContactUs from './Components/ContactUs/ContactUs'
+import About from './Components/About/About'
+import Donate from './Components/Donate/Donate'
+import AdminLogin from './Components/Admin/AdminLogin'
+import AdminBlogs from './Components/Admin/AdminBlogs'
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={
+        <Route
+          path="/"
+          element={
             <>
               <Hero />
-              <Carousel /> 
-              <KeyAchievements/>
-              <Team/>
-              <FAQ/>
-              <Invitation/>
+              <Carousel />
+              <KeyAchievements />
+              <Team />
+              <FAQ />
+              <Invitation />
             </>
           }
         />
+
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/blogs" element={<Blog />} />
         <Route path="/blogs/:id" element={<BlogDetail />} />
@@ -45,19 +48,9 @@ const App = () => {
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin/blogs" element={<AdminBlogs />} />
       </Routes>
+
       <Footer />
-    </Router>
-
-
-
-
-    // <div>
-    //   <Navbar />
-    //   <Hero />
-    //   <Gallery/>
-    //   <About/>
-      
-    // </div>
+    </>
   )
 }
 
